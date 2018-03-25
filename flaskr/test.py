94b154c8cd852cac73014fae26c671e4e4d38ef7
Burlesque <yangkun@vidaidentity.com>
2018-03-25 01:45:51 -0400

    Update test.py

--- flaskr/test.py
+++ flaskr/test.py
@@ -27,7 +27,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_connect(self):
         rv = self.app.get('/')
-        self.assertEqual(rv.status_code, 400)
+        self.assertEqual(rv.status_code, 200)
 
     def test_img_upload(self):
         
